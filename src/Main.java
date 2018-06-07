dad56430182c55caf39d1a2da737c00a4c3fb933
max101010 <max10@tut.by>
2018-06-07 14:39:32 +0300

    dfd

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,7 @@ import java.io.FileNotFoundException;
 public class Main {
 
 
-    public static void main(String[] args) {
-
-        //создаем директора
-        Director director = new Director();
-        CarPark carPark = new CarPark();
-        CarPark carPark1 = new CarPark();
-
-        //создаем строителей
-        CarBuilder builderTruck = new CarBuilder();
-        CarBuilder builderBus = new CarBuilder();
-        CarBuilder builderCar = new CarBuilder();
-
-        //выбираем, что будем конструировать
-        director.constructTruck(builderTruck);
-        director.constructBus(builderBus);
-        director.constructCar(builderCar);
-
-        //создаем экземпляр класса
-
-        Car bus = builderBus.getResult(Type.Bus);
-      //  bus.addCar();
-        Car auto = builderCar.getResult(Type.Auto);
-       // auto.addCar();
-        Car truck = builderTruck.getResult(Type.Truck);
-       //truck.addCar();
+    pu
 
       carPark.addCar(bus);
       carPark.addCar(auto);
